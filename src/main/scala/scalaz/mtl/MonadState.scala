e781d8577bfc7954c9a45c0bdce1c69e8c67664c
Pawel Szulc <paul.szulc@gmail.com>
2018-06-28 21:07:24 +0200

    Introduce initial MonadState

---
+++ src/main/scala/scalaz/mtl/MonadState.scala
@@ -0,0 +1,26 @@
+package scalaz
+package mtl
+
+import scalaz._
+
+trait MonadState[F[_], S] extends Serializable {
+  val monad: Monad[F]
+
+  def get: F[S]
+
+  def set(s: S): F[Unit]
+
+  def inspect[A](f: S => A): F[A]
+
+  def modify(f: S => S): F[Unit]
+}
+
+trait DefaultMonadState[F[_], S] extends MonadState[F, S] {
+  def inspect[A](f: S => A): F[A] = monad.map(get)(f)
+  def modify(f: S => S): F[Unit] = monad.flatMap(inspect(f))(set)
+}
+
+object MonadState {
+  def apply[F[_], S](implicit ev: MonadState[F, S]): MonadState[F, S] = ev
+
+}
