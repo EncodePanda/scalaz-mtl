0f9fb0635852ba1d58a11d1fa19e053e3b2b7b84
Pawel Szulc <paul.szulc@gmail.com>
2018-07-01 11:29:36 +0200

    Add MonadState for EitherT[F..] where F:MonadState:Monad

--- src/main/scala/scalaz/mtl/MonadState.scala
+++ src/main/scala/scalaz/mtl/MonadState.scala
@@ -1,9 +1,9 @@
 package scalaz
 package mtl
 
-import scalaz._
+import scalaz.{MonadState => _, _}
 
-trait MonadState[F[_], S] extends Serializable {
+trait MonadState[F[_], S] {
   val monad: Monad[F]
 
   def get: F[S]
@@ -20,7 +20,17 @@ trait DefaultMonadState[F[_], S] extends MonadState[F, S] {
   def modify(f: S => S): F[Unit] = monad.bind(inspect(f))(set)
 }
 
-object MonadState {
+object MonadState extends MonadState0 {
   def apply[F[_], S](implicit ev: MonadState[F, S]): MonadState[F, S] = ev
+}
 
+trait MonadState0 {
+  implicit def eitherTMonadState[F[_]: Monad: MonadState[?[_], S], S, E]
+      : MonadState[EitherT[F, E, ?], S] = new DefaultMonadState[EitherT[F, E, ?], S] {
+    val monad: Monad[EitherT[F, E, ?]] = Monad[EitherT[F, E, ?]]
+    def get: EitherT[F, E, S] =
+      MonadTrans[EitherT[?[_], E, ?]].liftM(MonadState[F, S].get)
+    def set(s: S): EitherT[F, E, Unit] =
+      MonadTrans[EitherT[?[_], E, ?]].liftM(MonadState[F, S].set(s))
+  }
 }
