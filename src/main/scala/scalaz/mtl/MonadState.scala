81b62702bc2c3b0564f608e274cd7ef750f060f5
Pawel Szulc <paul.szulc@gmail.com>
2018-07-01 15:28:15 +0200

    Add instance of MonadState for StateT[F..] where F:Monad

--- src/main/scala/scalaz/mtl/MonadState.scala
+++ src/main/scala/scalaz/mtl/MonadState.scala
@@ -24,7 +24,7 @@ object MonadState extends MonadState0 {
   def apply[F[_], S](implicit ev: MonadState[F, S]): MonadState[F, S] = ev
 }
 
-trait MonadState0 {
+trait MonadState0  extends MonadState1 {
   implicit def eitherTMonadState[F[_]: Monad: MonadState[?[_], S], S, E]
       : MonadState[EitherT[F, E, ?], S] = new DefaultMonadState[EitherT[F, E, ?], S] {
     val monad: Monad[EitherT[F, E, ?]] = Monad[EitherT[F, E, ?]]
@@ -32,5 +32,15 @@ trait MonadState0 {
       MonadTrans[EitherT[?[_], E, ?]].liftM(MonadState[F, S].get)
     def set(s: S): EitherT[F, E, Unit] =
       MonadTrans[EitherT[?[_], E, ?]].liftM(MonadState[F, S].set(s))
+  }
+}
+
+trait MonadState1 {
+  implicit def stateTMonadState[F[_]:Monad, S]
+      : MonadState[StateT[F, S, ?], S] =
+    new DefaultMonadState[StateT[F, S, ?], S] {
+    val monad: Monad[StateT[F, S, ?]] = Monad[StateT[F, S, ?]]
+    def get: StateT[F, S , S] = StateT(s => Monad[F].point((s, s)))
+    def set(s: S): StateT[F, S, Unit] = StateT(_ => Monad[F].point((s, ())))
   }
 }
